8c9112b743d96ad3b2aa977a6d7a8755510f7bec
lumiaqian <cao156@outlook.com>
2020-04-28 14:45:00 +0800

    完善基础

---
+++ blog-svc/src/main/java/com/caoyuqian/blogsvc/BlogSvcApplication.java
@@ -0,0 +1,22 @@
+package com.caoyuqian.blogsvc;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author lumiaqian
+ */
+@SpringBootApplication
+@MapperScan("com.caoyuqian.blogsvc.mapper")
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.caoyuqian.common","com.caoyuqian.user"})
+public class BlogSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSvcApplication.class, args);
+    }
+
+}
---
+++ blog-svc/src/test/java/com/caoyuqian/blogsvc/BlogSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caoyuqian.blogsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
